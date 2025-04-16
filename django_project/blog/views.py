c13a9c39d1e7359215c85c9ad3303bbd8a43561b
nlankwei5 <lampteyebenezer220@gmail.com>
2025-04-16 13:53:09 +0000

    Created database tables and also created forms to register new users

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
+from .models import Post 
 
-posts =[
-    {
-        'author': 'Lankwei',
-        'title': 'Blog Post 1',
-        'content': 'First Blog Post',
-        'date_posted': '15th April, 2025'
-    },
-     {
-        'author': 'Mami',
-        'title': 'Blog Post 2',
-        'content': 'Second Blog Post',
-        'date_posted': '15th April, 2025'
-    }
-]
+
 
 
 def home(request):
     context = {
-          'posts': posts
+          'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
+
 
 
 def about(request):
