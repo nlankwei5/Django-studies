b09fd136b547de16907eabacdd066b04580bff0b
nlankwei5 <lampteyebenezer220@gmail.com>
2025-04-16 00:36:10 +0000

    Created templates folder for html files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts =[
+    {
+        'author': 'Lankwei',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post',
+        'date_posted': '15th April, 2025'
+    },
+     {
+        'author': 'Mami',
+        'title': 'Blog Post 2',
+        'content': 'Second Blog Post',
+        'date_posted': '15th April, 2025'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+          'posts': posts
+    }
+    return render(request, 'blog/home.html', context) 
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'}) 
